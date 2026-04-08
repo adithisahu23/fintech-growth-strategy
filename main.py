5358fa39289ad6aa0b57a927cb7a9a3e81e7cecf
ADITI SAHU <0105cd231018@oriental.ac.in>
2026-04-09 00:27:28 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.churn_model import train_churn_model
-from src.segmentation import segment_users
-
-print("Training churn model...")
-train_churn_model()
-
-print("\nSegmenting users...")
-segment_users()
